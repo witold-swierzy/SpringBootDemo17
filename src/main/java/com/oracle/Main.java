38b464730e390abe2b6d9106f31b892c83b52b3b
Witold Swierzy <witold.swierzy@oracle.com>
2025-08-21 06:02:20 +0200

    Initial commit

---
+++ src/main/java/com/oracle/Main.java
@@ -0,0 +1,12 @@
+package com.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
